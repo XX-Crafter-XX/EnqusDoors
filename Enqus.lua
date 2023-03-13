43c0d9a16140e5697c00e9d6fcc1f6b7ac75d2bd
XX-Crafter-XX <91060445+XX-Crafter-XX@users.noreply.github.com>
2023-03-13 14:29:45 +0100

    Create Enqus.lua

---
+++ Enqus.lua
@@ -0,0 +1,243 @@
+local EnqusDoors = Instance.new("ScreenGui")
+local EnqusFrame = Instance.new("Frame")
+local InfiniteYield = Instance.new("TextButton")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local UICorner_2 = Instance.new("UICorner")
+local UICorner_3 = Instance.new("UICorner")
+local PoopDoors = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local DarkraiX = Instance.new("TextButton")
+local UICorner_5 = Instance.new("UICorner")
+local ProjectWD = Instance.new("TextButton")
+local UICorner_6 = Instance.new("UICorner")
+local Dansploit = Instance.new("TextButton")
+local UICorner_7 = Instance.new("UICorner")
+local Vynixius = Instance.new("TextButton")
+local UICorner_8 = Instance.new("UICorner")
+
+--Properties:
+
+EnqusDoors.Name = "EnqusDoors"
+EnqusDoors.Parent = (game:GetService("CoreGui") or gethui())
+EnqusDoors.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+EnqusFrame.Name = "EnqusFrame"
+EnqusFrame.Parent = EnqusDoors
+EnqusFrame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+EnqusFrame.Position = UDim2.new(0.0155535229, 0, 0.0189873409, 0)
+EnqusFrame.Size = UDim2.new(0, 174, 0, 292)
+
+InfiniteYield.Name = "InfiniteYield"
+InfiniteYield.Parent = EnqusFrame
+InfiniteYield.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
+InfiniteYield.BorderColor3 = Color3.fromRGB(134, 134, 134)
+InfiniteYield.Position = UDim2.new(0, 0, 0.158323541, 0)
+InfiniteYield.Size = UDim2.new(0, 174, 0, 39)
+InfiniteYield.Font = Enum.Font.SourceSans
+InfiniteYield.Text = "Start Infinite Yield"
+InfiniteYield.TextColor3 = Color3.fromRGB(0, 0, 0)
+InfiniteYield.TextSize = 14.000
+
+UICorner.Parent = InfiniteYield
+
+TextLabel.Parent = EnqusFrame
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Size = UDim2.new(0, 174, 0, 39)
+TextLabel.Font = Enum.Font.SourceSans
+TextLabel.Text = "EnqusDoors"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 14.000
+
+UICorner_2.Parent = TextLabel
+
+UICorner_3.Parent = EnqusFrame
+
+PoopDoors.Name = "PoopDoors"
+PoopDoors.Parent = EnqusFrame
+PoopDoors.BackgroundColor3 = Color3.fromRGB(113, 113, 113)
+PoopDoors.BorderColor3 = Color3.fromRGB(113, 113, 113)
+PoopDoors.Position = UDim2.new(0, 0, 0.289502859, 0)
+PoopDoors.Size = UDim2.new(0, 174, 0, 39)
+PoopDoors.Font = Enum.Font.SourceSans
+PoopDoors.Text = "Start PoopDoors"
+PoopDoors.TextColor3 = Color3.fromRGB(0, 0, 0)
+PoopDoors.TextSize = 14.000
+
+UICorner_4.Parent = PoopDoors
+
+DarkraiX.Name = "DarkraiX"
+DarkraiX.Parent = EnqusFrame
+DarkraiX.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
+DarkraiX.BorderColor3 = Color3.fromRGB(134, 134, 134)
+DarkraiX.Position = UDim2.new(0, 0, 0.423020393, 0)
+DarkraiX.Size = UDim2.new(0, 174, 0, 39)
+DarkraiX.Font = Enum.Font.SourceSans
+DarkraiX.Text = "Start Darkrai X"
+DarkraiX.TextColor3 = Color3.fromRGB(0, 0, 0)
+DarkraiX.TextSize = 14.000
+
+UICorner_5.Parent = DarkraiX
+
+ProjectWD.Name = "ProjectWD"
+ProjectWD.Parent = EnqusFrame
+ProjectWD.BackgroundColor3 = Color3.fromRGB(113, 113, 113)
+ProjectWD.BorderColor3 = Color3.fromRGB(113, 113, 113)
+ProjectWD.Position = UDim2.new(0, 0, 0.554622114, 0)
+ProjectWD.Size = UDim2.new(0, 174, 0, 39)
+ProjectWD.Font = Enum.Font.SourceSans
+ProjectWD.Text = "Start ProjectWD"
+ProjectWD.TextColor3 = Color3.fromRGB(0, 0, 0)
+ProjectWD.TextSize = 14.000
+
+UICorner_6.Parent = ProjectWD
+
+Dansploit.Name = "Dansploit"
+Dansploit.Parent = EnqusFrame
+Dansploit.BackgroundColor3 = Color3.fromRGB(134, 134, 134)
+Dansploit.BorderColor3 = Color3.fromRGB(134, 134, 134)
+Dansploit.Position = UDim2.new(0, 0, 0.684886217, 0)
+Dansploit.Size = UDim2.new(0, 174, 0, 39)
+Dansploit.Font = Enum.Font.SourceSans
+Dansploit.Text = "Start DanSploit"
+Dansploit.TextColor3 = Color3.fromRGB(0, 0, 0)
+Dansploit.TextSize = 14.000
+
+UICorner_7.Parent = Dansploit
+
+Vynixius.Name = "Vynixius"
+Vynixius.Parent = EnqusFrame
+Vynixius.BackgroundColor3 = Color3.fromRGB(113, 113, 113)
+Vynixius.BorderColor3 = Color3.fromRGB(113, 113, 113)
+Vynixius.Position = UDim2.new(0, 0, 0.815023243, 0)
+Vynixius.Size = UDim2.new(0, 174, 0, 39)
+Vynixius.Font = Enum.Font.SourceSans
+Vynixius.Text = "Start Vynixius"
+Vynixius.TextColor3 = Color3.fromRGB(0, 0, 0)
+Vynixius.TextSize = 14.000
+
+UICorner_8.Parent = Vynixius
+
+-- Scripts:
+
+local function LTIIHN_fake_script() -- InfiniteYield.InfiniteYield 
+	local script = Instance.new('LocalScript', InfiniteYield)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/InfiniteYield.lua'))()
+	end)
+	
+end
+coroutine.wrap(LTIIHN_fake_script)()
+local function HSXJCXD_fake_script() -- EnqusFrame.DRAGUI 
+	local script = Instance.new('LocalScript', EnqusFrame)
+
+	--ITS FREEE
+	local UIS = game:GetService("UserInputService")
+	function DRAGUI(Frame)
+		dragToggle = nil
+		local dragSpeed = 0.5
+		dragInput = nil
+		dragStart = nil
+		local dragPos = nil
+		function updateInput(input)
+			local Delta = input.Position - dragStart
+			local Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Frame, TweenInfo.new(0.25), {Position = Position}):Play()
+		end
+		Frame.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) and UIS:GetFocusedTextBox() == nil then
+				dragToggle = true
+				dragStart = input.Position
+				startPos = Frame.Position
+				input.Changed:Connect(function()
+					if input.UserInputState == Enum.UserInputState.End then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+		Frame.InputChanged:Connect(function(input)
+			if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+				dragInput = input
+			end
+		end)
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if input == dragInput and dragToggle then
+				updateInput(input)
+			end
+		end)
+	end
+	
+	DRAGUI(script.Parent)
+end
+coroutine.wrap(HSXJCXD_fake_script)()
+local function YOKCY_fake_script() -- PoopDoors.PoopDoors 
+	local script = Instance.new('LocalScript', PoopDoors)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/PoopDoors.lua'))()
+	end)
+	
+end
+coroutine.wrap(YOKCY_fake_script)()
+local function BTVIBR_fake_script() -- DarkraiX.DarkraiX 
+	local script = Instance.new('LocalScript', DarkraiX)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/Darkrai.lua'))()
+	end)
+	
+end
+coroutine.wrap(BTVIBR_fake_script)()
+local function EKET_fake_script() -- ProjectWD.ProjectWD 
+	local script = Instance.new('LocalScript', ProjectWD)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/ProjectWD.lua'))()
+	end)
+	
+end
+coroutine.wrap(EKET_fake_script)()
+local function SOQJ_fake_script() -- Dansploit.DanSploit 
+	local script = Instance.new('LocalScript', Dansploit)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/Dansploit.lua'))()
+	end)
+	
+end
+coroutine.wrap(SOQJ_fake_script)()
+local function IJYL_fake_script() -- Vynixius.Vynixius 
+	local script = Instance.new('LocalScript', Vynixius)
+
+	local button = script.Parent
+	
+	
+	
+	button.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/XX-Crafter-XX/RBXLLUA/main/Vynixius.lua'))()
+	end)
+	
+end
+coroutine.wrap(IJYL_fake_script)()
